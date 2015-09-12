d2dad0a76b224b72550dc6ff5f23848f11c0c741
Bryce Billing <bllbry001@myuct.ac.za>
2015-09-12 15:16:57 +0200

    Added rotation to the troops movement

--- Assets/AIScripts/UnitMonitor.cs
+++ Assets/AIScripts/UnitMonitor.cs
@@ -4,7 +4,6 @@ using System.Collections;
 public class UnitMonitor : MonoBehaviour {
 	public static ArrayList selectedUnits = new ArrayList();
 	static int MAX_UNITS_SELECTED = 25; 
-
 	public static void RemoveUnit(GameObject unit){
 		for (int i =0; i<selectedUnits.Count; i++) {
 			GameObject unitInArrayList = selectedUnits[i] as GameObject;
@@ -27,4 +26,6 @@ public class UnitMonitor : MonoBehaviour {
 	public static bool isShiftPressed(){
 		return (Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift));
 	}
+
+
 }
