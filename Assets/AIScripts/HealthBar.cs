0048cb5fc906bc49ac8ea687a42d3513356c3dcf
Bryce Billing <brycerbilling@gmail.com>
2015-10-06 11:44:57 +0200

    Added working health bars

---
+++ Assets/AIScripts/HealthBar.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HealthBar : MonoBehaviour {
+	public GameObject healthBar;
+	public int MAX_HEALTH = 100;
+	public void SetHealthBar(float health){
+		healthBar.transform.localScale = new Vector3 (Mathf.Clamp(health/MAX_HEALTH,0f ,1f),healthBar.transform.localScale.y,healthBar.transform.localScale.z);
+	}
+}
