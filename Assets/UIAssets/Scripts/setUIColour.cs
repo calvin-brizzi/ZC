a6cd2ced9fd659537ca13c5c4edf19da3803b2a9
Stuart McRoberts <strt.mcroberts@gmail.com>
2015-10-10 21:22:38 +0200

    Cleaned stuff up and back up push

---
+++ Assets/UIAssets/Scripts/setUIColour.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class setUIColour : MonoBehaviour {
+	public GameObject daddy;
+	int team;
+	public Sprite[] tex;
+	// Use this for initialization
+	void Start () {
+		team = daddy.GetComponent<DestructableBuilding> ().team;
+
+		SpriteRenderer rend = GetComponent<SpriteRenderer>();
+		rend.sprite = tex[team - 1];
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
