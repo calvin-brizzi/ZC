90a0bcb4ad2947661c592733761428c392abeebc
Stuart McRoberts <strt.mcroberts@gmail.com>
2015-10-19 20:23:59 +0200

    added camera orientations

---
+++ Assets/Orientate.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Orientate : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		if (VarMan.Instance.pNum != 2) {
+			Quaternion target = Quaternion.Euler (0,90,0);
+			this.transform.eulerAngles = new Vector3(
+				this.transform.eulerAngles.x,
+				this.transform.eulerAngles.y + 180,
+				this.transform.eulerAngles.z
+				);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/cameraPositioning.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class cameraPositioning : MonoBehaviour {
+	public GameObject redBase;
+	public GameObject blueBase;
+	// Use this for initialization
+	void Start () {
+		Vector3 temp = this.transform.position;
+		temp.y = temp.y + 10;
+		if (VarMan.Instance.pNum != 1) {
+			temp.x = redBase.transform.position.x;
+			temp.z = redBase.transform.position.z;
+		} else {
+			temp.x = blueBase.transform.position.x;
+			temp.z = blueBase.transform.position.z;
+		}
+		this.transform.position = temp;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
